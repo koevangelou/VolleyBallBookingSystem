775f393dad0958dc92afa5e312545491105f1fbd
Evangelou <koevangelou@deloitte.gr>
2024-10-09 12:20:19 +0300

    commenting a using statement in dbcontext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore; 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography.X509Certificates;
